b91f7085d601ffde5d81db21e094bf281cde26f4
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2023-11-01 21:48:32 -0300

    Create timeJogador.php

---
+++ Time Futebol/timeJogador.php
@@ -0,0 +1,47 @@
+<?php 
+    class time {
+        private $nome;
+        private $anoFundacao;
+        private $jogadores = array();
+
+        public function __construct(){
+            $this->jogadores;
+        }
+        public function getJogadores() {
+            return $this->jogadores;
+        }
+
+        public function addJogadores($oJogador) {
+            $array_push($this->jogadores, $oJogador);
+        }
+    }
+
+    class jogador {
+        private $nome;
+        private $posicao;
+        public function getNome(){
+            return $this->nome;
+        }    
+        public function setNome($nome) {
+            $this->nome = $nome;
+        }
+        public function getPosicao() {
+            return $this->posicao;
+        }
+        public function setPosicao($posicao) {
+            $this->posicao = $posicao;
+        }
+    }
+
+    $brasil = new time();
+    $brasil->setNome ("Brasil Futsal");
+
+    $jogador = new jogador();
+    $jogador->setNome ("João"); 
+    $jogador->setPosicao ("Goleiroo");
+
+    $jogador = new jogador();
+    $jogador->setNome ("gabriel"); 
+    $jogador->setPosicao ("Atacante");
+
+?>
