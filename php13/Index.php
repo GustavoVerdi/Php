370eba8b56c00cdb042788eda712073cea7a2f8c
Gustavo Verdi <gustavoverdi2015@gmail.com>
2023-11-01 21:26:17 -0300

    New

--- php13/Index.php
+++ php13/Index.php
@@ -8,9 +8,9 @@
 
     $pessoa->getTelefone()->setTipo(1);
     $pessoa->getTelefone()->setNome("Tel Celular");
-    $pessoa->getTelefone()->setValor("(47) 99999-0000");
+    $pessoa->getTelefone()->setValor("(47) 99970-7572");
 
-    $pessoa->getEndereco()->setLogradouro("Estrada Blumenau");
+    $pessoa->getEndereco()->setLogradouro("Rua 7 de setembro");
     $pessoa->getEndereco()->setBairro("Brehmer");
     $pessoa->getEndereco()->setCidade("Rio do Sul");
     $pessoa->getEndereco()->setCEP("89160-000");
