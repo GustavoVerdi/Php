b567ae70b897e3fd1955a21e01a8efa034ae86d7
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2023-11-01 21:28:16 -0300

    Update computador.php

--- Computador/computador.php
+++ Computador/computador.php
@@ -1,4 +1,21 @@
 <?php
+    class computador {
+        private $estadoAtual;
+        public function liga() {
+            $this->estadoAtual = "Ligado";
+        }
+        public function desliga() {
+            $this->estadoAtual = "Desligado";
+        }
+        public function getestadoAtual() {
+            return $this->estadoAtual;
+        }
+    }
 
+    $comp = new computador();
+    $comp->liga();
+    echo "Liga: " . $comp->getestadoAtual() . "<br>";
+    $comp->desliga();
+    echo "Desliga: " . $comp->getestadoAtual() . "<br>";
 
 ?>
