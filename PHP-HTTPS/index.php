c9a9a5e709b1a39359e92e04d80cb2f135d776dd
Gustavo Verdi <gustavoverdi2015@gmail.com>
2024-03-14 18:57:58 -0300

    aTT

---
+++ PHP-HTTPS/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="processa_formulario.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+
+        <label for="telefone">Telefone:</label>
+        <input type="tel" id="telefone" name="telefone" required>
+
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="mensagem">Mensagem:</label>
+        <textarea id="mensagem" name="mensagem" rows="4" required></textarea>
+
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html> 
